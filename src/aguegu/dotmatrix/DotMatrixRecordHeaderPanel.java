43796ba19130fb6bfa5a40b5192f219766a26331
Weihong.Guan <weihong.Guan@gmail.com>
2012-11-17 23:29:10 +0800

    add header panel

---
+++ src/aguegu/dotmatrix/DotMatrixRecordHeaderPanel.java
@@ -0,0 +1,208 @@
+package aguegu.dotmatrix;
+
+import java.awt.Dimension;
+import java.awt.FlowLayout;
+import java.awt.Insets;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import javax.swing.BoxLayout;
+import javax.swing.ButtonGroup;
+import javax.swing.JCheckBox;
+import javax.swing.JPanel;
+import javax.swing.JRadioButton;
+import javax.swing.JSlider;
+import javax.swing.border.EmptyBorder;
+import javax.swing.event.ChangeEvent;
+import javax.swing.event.ChangeListener;
+
+public class DotMatrixRecordHeaderPanel extends JPanel
+{
+	private static final long serialVersionUID = 7530602456593370095L;
+
+	private DotMatrixRecordPanel parent;
+	private DotMatrixRecordFrame dmrf;
+
+	private JSlider sliderBrightness;
+	private JSlider sliderSmallSpan;
+	private JSlider sliderBigSpan;
+
+	private JCheckBox checkboxUpperLed;
+	private JCheckBox checkboxBottomLed;
+
+	private JRadioButton[] radiobuttonModes;
+
+	private CL cl;
+
+	public DotMatrixRecordHeaderPanel(DotMatrixRecordPanel dmrp)
+	{
+		parent = dmrp;
+
+		this.setLayout(new BoxLayout(this, BoxLayout.Y_AXIS));
+		this.setBorder(new EmptyBorder(new Insets(0, 4, 0, 0)));
+		// this.setAlignmentX(LEFT_ALIGNMENT);
+
+		JPanel panelMode = new JPanel(new FlowLayout(FlowLayout.LEFT));
+		radiobuttonModes = new JRadioButton[3];
+		ButtonGroup bgMode = new ButtonGroup();
+		int i = 0;
+		for (DMMode mode : DMMode.values())
+		{
+			radiobuttonModes[i] = new JRadioButton(mode.toString());
+			radiobuttonModes[i].setActionCommand(mode.toString());
+			radiobuttonModes[i].addActionListener(new ActionListenerMode());
+			bgMode.add(radiobuttonModes[i]);
+			panelMode.add(radiobuttonModes[i]);
+			i++;
+		}
+		panelMode.setAlignmentX(LEFT_ALIGNMENT);
+		this.add(panelMode);
+
+		JPanel panelAttachment = new JPanel(new FlowLayout(FlowLayout.LEFT));
+		checkboxUpperLed = new JCheckBox("Upper Led");
+		checkboxUpperLed.addActionListener(new ActionListenerAttachment());
+		panelAttachment.add(checkboxUpperLed);
+
+		checkboxBottomLed = new JCheckBox("Bottom Led");
+		checkboxBottomLed.addActionListener(new ActionListenerAttachment());
+		panelAttachment.add(checkboxBottomLed);
+
+		panelAttachment.setAlignmentX(LEFT_ALIGNMENT);
+		this.add(panelAttachment);
+
+		cl = new CL();
+
+		sliderBrightness = new JSlider(0, 255, 255);
+		sliderBrightness.setMinorTickSpacing(0x20);
+		sliderBrightness.setMaximumSize(new Dimension(180, 0));
+		sliderBrightness.setPaintTicks(true);
+		sliderBrightness.setSnapToTicks(true);
+		sliderBrightness.addChangeListener(cl);
+
+		JPanel panelBrightness = new JPanel(new FlowLayout(FlowLayout.LEFT));
+		panelBrightness.add(sliderBrightness);
+		panelBrightness.setAlignmentX(LEFT_ALIGNMENT);
+		this.add(panelBrightness);
+
+		sliderSmallSpan = new JSlider(0, 65535, 65535);
+		sliderSmallSpan.setMinorTickSpacing(0x1000);
+		sliderSmallSpan.setMaximumSize(new Dimension(180, 0));
+		sliderSmallSpan.setSnapToTicks(true);
+		sliderSmallSpan.setPaintTicks(true);
+		sliderSmallSpan.addChangeListener(cl);
+
+		sliderBigSpan = new JSlider(0, 65535, 0);
+		sliderBigSpan.setMinorTickSpacing(0x1000);
+		sliderBigSpan.setMaximumSize(new Dimension(180, 0));
+		sliderBigSpan.setSnapToTicks(true);
+		sliderBigSpan.setPaintTicks(true);
+		sliderBigSpan.addChangeListener(cl);
+
+		JPanel panelSpan = new JPanel(new FlowLayout(FlowLayout.LEFT));
+
+		panelSpan.add(sliderSmallSpan);
+		panelSpan.add(sliderBigSpan);
+		panelSpan.setAlignmentX(LEFT_ALIGNMENT);
+
+		this.add(panelSpan);
+
+		// System.out.println(sliderBrightness.getPreferredSize());
+
+	}
+
+	private class CL implements ChangeListener
+	{
+		@Override
+		public void stateChanged(ChangeEvent e)
+		{
+			if (e.getSource() instanceof JSlider)
+			{
+				if (e.getSource().equals(sliderBrightness))
+				{
+					dmrf.setBrightness((Integer) sliderBrightness.getValue());
+				}
+				else if (e.getSource().equals(sliderSmallSpan))
+				{
+					dmrf.setSmallSpan((Integer) sliderSmallSpan.getValue());
+				}
+				else if (e.getSource().equals(sliderBigSpan))
+				{
+					dmrf.setBigSpan((Integer) sliderBigSpan.getValue());
+				}
+
+				parent.refresh(true);
+			}
+		}
+	}
+
+	private class ActionListenerAttachment implements ActionListener
+	{
+		@Override
+		public void actionPerformed(ActionEvent e)
+		{
+			if (checkboxUpperLed.isSelected() && checkboxBottomLed.isSelected())
+				dmrf.setAttachment(DMAttachment.BOTH);
+			else if (checkboxUpperLed.isSelected())
+				dmrf.setAttachment(DMAttachment.UPPER_LED);
+			else if (checkboxBottomLed.isSelected())
+				dmrf.setAttachment(DMAttachment.BOTTOM_LED);
+			else
+				dmrf.setAttachment(DMAttachment.NONE);
+			parent.refresh(true);
+		}
+	}
+
+	public void setDotMatrixRecordFrame(DotMatrixRecordFrame dmrf)
+	{
+		this.dmrf = dmrf;
+	}
+
+	private class ActionListenerMode implements ActionListener
+	{
+		@Override
+		public void actionPerformed(ActionEvent e)
+		{
+			dmrf.setMode(DMMode.getMode(e.getActionCommand()));
+			parent.refresh(true);
+		}
+	}
+
+	public void refresh()
+	{
+		sliderBrightness.removeChangeListener(cl);
+		sliderBrightness.setValue(dmrf.getBrightness());
+		sliderBrightness.addChangeListener(cl);
+
+		sliderSmallSpan.removeChangeListener(cl);
+		sliderSmallSpan.setValue(dmrf.getSmallSpan());
+		sliderSmallSpan.addChangeListener(cl);
+
+		sliderBigSpan.removeChangeListener(cl);
+		sliderBigSpan.setValue(dmrf.getBigSpan());
+		sliderBigSpan.addChangeListener(cl);
+
+		switch (dmrf.getAttachment())
+		{
+		case BOTH:
+			checkboxUpperLed.setSelected(true);
+			checkboxBottomLed.setSelected(true);
+			break;
+		case BOTTOM_LED:
+			checkboxUpperLed.setSelected(false);
+			checkboxBottomLed.setSelected(true);
+			break;
+		case UPPER_LED:
+			checkboxUpperLed.setSelected(true);
+			checkboxBottomLed.setSelected(false);
+			break;
+		case NONE:
+			checkboxUpperLed.setSelected(false);
+			checkboxBottomLed.setSelected(false);
+		default:
+			break;
+		}
+
+		radiobuttonModes[dmrf.getMode().ordinal()].setSelected(true);
+	}
+
+}

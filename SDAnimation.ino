c50644126dc5efb7329f3b94e0409f29ae7a446d
Weihong.Guan <weihong.Guan@gmail.com>
2013-03-12 18:38:47 +0800

    add arduino program to read animation from SD and sending to 3D8

---
+++ SDAnimation.ino
@@ -0,0 +1,79 @@
+#include "SD.h"
+#include "string.h"
+
+Sd2Card card;
+SdVolume volume;
+const int chipSelect = 4;
+File root;
+
+void setup()
+{
+	Serial.begin(57600);
+
+	pinMode(10, OUTPUT); // change this to 53 on a mega
+
+	bool sd_ok = SD.begin(9);
+
+	if (!sd_ok)
+	{
+		Serial.println("failed.");
+		return;
+	}
+
+	root = SD.open("/");
+}
+
+void sendAnimation(byte *data)
+{
+	Serial.write(0xf3);
+	Serial.write(data[1]);
+
+	Serial.write(0xf4);
+	Serial.write(data[2]);
+
+	Serial.write(0xf5);
+	Serial.write(data[3]);
+
+	Serial.write(0xf2);
+	for (byte i = 8; i < 72; i++)
+	{
+		Serial.write(data[i]);
+	}
+
+	delayMicroseconds(makeWord(data[4], data[5]));
+	delay(makeWord(data[6], data[7]));
+}
+
+void readAnimation(File & file)
+{
+	byte data[72];
+	while (file.available())
+	{
+		file.read(data, 72);
+//		for (byte i = 0; i < 72; i++)
+//			Serial.write(data[i]);
+
+		sendAnimation(data);
+	}
+}
+
+void loop(void)
+{
+	File file = root.openNextFile();
+	if (file)
+	{
+		char *p = file.name();
+		char *p_dot = strchr(p, '.');
+
+		if (p_dot != NULL && strcmp(p_dot, ".DAT") == 0)
+		{
+			readAnimation(file);
+		}
+
+		file.close();
+	}
+	else
+		root.rewindDirectory();
+}
+
+
